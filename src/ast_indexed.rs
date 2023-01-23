0b60aa8e54db2b06ac2992346efe249a8abd67b0
miralushch <miralushch@gmail.com>
2023-01-23 07:24:03 +0700

    fix label after jump

--- src/ast_indexed.rs
+++ src/ast_indexed.rs
@@ -18,10 +18,10 @@ pub(super) enum AstIndexed {
     Max(Box<AstIndexed>, Box<AstIndexed>),
     Min(Box<AstIndexed>, Box<AstIndexed>),
     Swap(u8, u8),
-    Label(usize),
-    Goto(usize),
-    GotoIf(usize, Box<AstIndexed>),
-    GotoIfNot(usize, Box<AstIndexed>),
+    Label(String),
+    Goto(String),
+    GotoIf(String, Box<AstIndexed>),
+    GotoIfNot(String, Box<AstIndexed>),
 }
 
 struct State {
@@ -31,9 +31,8 @@ struct State {
 impl From<Ast> for AstIndexed {
     fn from(ast: Ast) -> AstIndexed {
         let memmgr = Rc::new(RefCell::new(HashMap::new()));
-        let lblmgr = Rc::new(RefCell::new(HashMap::new()));
         let state = Rc::new(RefCell::new(State { _while: Vec::new() }));
-        AstIndexed::new(ast, memmgr, lblmgr, state)
+        AstIndexed::new(ast, memmgr, state)
     }
 }
 
@@ -41,7 +40,6 @@ impl AstIndexed {
     fn new(
         ast: Ast,
         memmgr: Rc<RefCell<HashMap<String, u8>>>,
-        lblmgr: Rc<RefCell<HashMap<String, usize>>>,
         state: Rc<RefCell<State>>,
     ) -> AstIndexed {
         match ast {
@@ -49,7 +47,7 @@ impl AstIndexed {
                 let root = AstIndexed::Root(
                     inner
                         .into_iter()
-                        .map(|inst| AstIndexed::new(inst, memmgr.clone(), lblmgr.clone(), state.clone()))
+                        .map(|inst| AstIndexed::new(inst, memmgr.clone(), state.clone()))
                         .collect(),
                 );
                 if !state.borrow()._while.is_empty() {
@@ -60,7 +58,7 @@ impl AstIndexed {
             Ast::Value(v) => AstIndexed::Value(v),
             Ast::Idnt(name) => AstIndexed::Indx(AstIndexed::get(name, memmgr)),
             Ast::Assign(var_name, inner) => {
-                let inner = Box::new(AstIndexed::new(*inner, memmgr.clone(), lblmgr, state));
+                let inner = Box::new(AstIndexed::new(*inner, memmgr.clone(), state));
                 AstIndexed::Assign(
                     AstIndexed::assign(var_name, memmgr),
                     inner,
@@ -69,70 +67,70 @@ impl AstIndexed {
             Ast::Input => AstIndexed::Input,
             Ast::Print(args) => AstIndexed::Print(
                 args.into_iter()
-                    .map(|arg| AstIndexed::new(arg, memmgr.clone(), lblmgr.clone(), state.clone()))
+                    .map(|arg| AstIndexed::new(arg, memmgr.clone(), state.clone()))
                     .collect(),
             ),
             Ast::Add(inner1, inner2) => AstIndexed::Add(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Sub(inner1, inner2) => AstIndexed::Sub(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Mul(inner1, inner2) => AstIndexed::Mul(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Div(inner1, inner2) => AstIndexed::Div(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Mod(inner1, inner2) => AstIndexed::Mod(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Max(inner1, inner2) => AstIndexed::Max(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Min(inner1, inner2) => AstIndexed::Min(
-                Box::new(AstIndexed::new(*inner1, memmgr.clone(), lblmgr.clone(), state.clone())),
-                Box::new(AstIndexed::new(*inner2, memmgr, lblmgr, state))
+                Box::new(AstIndexed::new(*inner1, memmgr.clone(), state.clone())),
+                Box::new(AstIndexed::new(*inner2, memmgr, state))
             ),
             Ast::Swap(var1, var2) => AstIndexed::Swap(AstIndexed::assign(var1, memmgr.clone()), AstIndexed::assign(var2, memmgr)),
-            Ast::Label(name) => AstIndexed::Label(AstIndexed::assign_lbl(name, lblmgr)),
-            Ast::Goto(name) => AstIndexed::Goto(AstIndexed::get_lbl(name, lblmgr)),
+            Ast::Label(name) => AstIndexed::Label(name),
+            Ast::Goto(name) => AstIndexed::Goto(name),
             Ast::GotoIf(name, cond) => AstIndexed::GotoIf(
-                AstIndexed::get_lbl(name, lblmgr.clone()),
-                Box::new(AstIndexed::new(*cond, memmgr, lblmgr, state)),
+                name,
+                Box::new(AstIndexed::new(*cond, memmgr, state)),
             ),
             Ast::GotoIfNot(name, cond) => AstIndexed::GotoIfNot(
-                AstIndexed::get_lbl(name, lblmgr.clone()),
-                Box::new(AstIndexed::new(*cond, memmgr, lblmgr, state)),
+                name,
+                Box::new(AstIndexed::new(*cond, memmgr, state)),
             ),
             Ast::While(cond) => {
                 let mut local_state = state.borrow_mut();
-                let icond = AstIndexed::new(*cond, memmgr, lblmgr.clone(), state.clone());
+                let icond = AstIndexed::new(*cond, memmgr, state.clone());
                 let name = format!("while_{icond:?}");
                 local_state._while.push((Box::new(icond), true));
-                AstIndexed::Label(AstIndexed::assign_lbl(name, lblmgr))
+                AstIndexed::Label(name)
             }
             Ast::WhileNot(cond) => {
                 let mut local_state = state.borrow_mut();
-                let icond = AstIndexed::new(*cond, memmgr, lblmgr.clone(), state.clone());
+                let icond = AstIndexed::new(*cond, memmgr, state.clone());
                 let name = format!("while_{icond:?}");
                 local_state._while.push((Box::new(icond), false));
-                AstIndexed::Label(AstIndexed::assign_lbl(name, lblmgr))
+                AstIndexed::Label(name)
             }
             Ast::Elihw => {
                 let mut local_state = state.borrow_mut();
                 let (cond, ty) = local_state._while.pop().expect("there are more elihws then whiles");
                 let name = format!("while_{cond:?}");
                 if ty {
-                    AstIndexed::GotoIf(AstIndexed::get_lbl(name, lblmgr), cond)
+                    AstIndexed::GotoIf(name, cond)
                 } else {
-                    AstIndexed::GotoIfNot(AstIndexed::get_lbl(name, lblmgr), cond)
+                    AstIndexed::GotoIfNot(name, cond)
                 }
             }
         }
@@ -155,26 +153,6 @@ impl AstIndexed {
             *n
         } else {
             panic!("uninitialized variable: {name}")
-        }
-    }
-
-    fn assign_lbl(name: String, lblmgr: Rc<RefCell<HashMap<String, usize>>>) -> usize {
-        let mut local_lblmgr = lblmgr.borrow_mut();
-        if let Some(n) = local_lblmgr.get(&name) {
-            *n
-        } else {
-            let n = local_lblmgr.len();
-            local_lblmgr.insert(name, n);
-            n
-        }
-    }
-
-    fn get_lbl(name: String, lblmgr: Rc<RefCell<HashMap<String, usize>>>) -> usize {
-        let local_lblmgr = lblmgr.borrow_mut();
-        if let Some(n) = local_lblmgr.get(&name) {
-            *n
-        } else {
-            panic!("uninitialized label: {name}")
         }
     }
 }
--- src/ir.rs
+++ src/ir.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use super::ast_indexed::AstIndexed;
 use mpl_vm::Instructions;
 
-pub(super) enum IrInst {
+enum IrInst {
     Psh(f64),
     Pfa(u8),
     Pta(u8),
@@ -17,10 +17,30 @@ pub(super) enum IrInst {
     Max,
     Min,
     Swap(u8, u8),
-    Label(usize),
-    Jmp(usize),
-    Jiz(usize),
-    Jnz(usize),
+    Label(String),
+    Jmp(String),
+    Jiz(String),
+    Jnz(String),
+}
+
+enum IrInst2 {
+    Psh(f64),
+    Sap(u8),
+    Pfa,
+    Pta,
+    Pek,
+    Pop,
+    Inp,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Max,
+    Min,
+    Jmp(String),
+    Jiz(String),
+    Jnz(String),
 }
 
 pub(super) struct Ir(Vec<IrInst>);
@@ -38,10 +58,10 @@ impl IrInst {
         match ai {
             AstIndexed::Root(inner) => inner.iter().for_each(|inst| IrInst::new(inst, ir)),
             AstIndexed::Value(val) => ir.push(IrInst::Psh(*val)),
-            AstIndexed::Indx(id) => ir.push(IrInst::Pfa(*id)),
+            AstIndexed::Indx(id) => ir.push(IrInst::Pfa(id.clone())),
             AstIndexed::Assign(id, inner) => {
                 IrInst::new(inner, ir);
-                ir.push(IrInst::Pta(*id))
+                ir.push(IrInst::Pta(id.clone()))
             }
             AstIndexed::Input => ir.push(IrInst::Inp),
             AstIndexed::Print(inner) => inner.iter().for_each(|inst| {
@@ -83,57 +103,57 @@ impl IrInst {
                 IrInst::new(inner2, ir);
                 ir.push(IrInst::Min)
             }
-            AstIndexed::Swap(id0, id1) => ir.push(IrInst::Swap(*id0, *id1)),
-            AstIndexed::Label(id) => ir.push(IrInst::Label(*id)),
-            AstIndexed::Goto(id) => ir.push(IrInst::Jmp(*id)),
+            AstIndexed::Swap(id0, id1) => ir.push(IrInst::Swap(id0.clone(), id1.clone())),
+            AstIndexed::Label(id) => ir.push(IrInst::Label(id.clone())),
+            AstIndexed::Goto(id) => ir.push(IrInst::Jmp(id.clone())),
             AstIndexed::GotoIf(id, inner) => {
                 IrInst::new(inner, ir);
-                ir.push(IrInst::Jiz(*id))
+                ir.push(IrInst::Jiz(id.clone()))
             }
             AstIndexed::GotoIfNot(id, inner) => {
                 IrInst::new(inner, ir);
-                ir.push(IrInst::Jnz(*id))
+                ir.push(IrInst::Jnz(id.clone()))
             }
         }
     }
 
-    fn codegen(&self, prog: &mut Vec<Instructions>, lblmgr: &mut HashMap<usize, usize>) {
-        match *self {
-            IrInst::Psh(val) => prog.push(Instructions::Psh(val)),
+    fn codegen(&self, prog: &mut Vec<IrInst2>, lblmgr: &mut HashMap<String, usize>) {
+        match self {
+            IrInst::Psh(val) => prog.push(IrInst2::Psh(*val)),
             IrInst::Pfa(id) => {
-                prog.push(Instructions::Sap(id));
-                prog.push(Instructions::Pfa)
+                prog.push(IrInst2::Sap(id.clone()));
+                prog.push(IrInst2::Pfa)
             }
             IrInst::Pta(id) => {
-                prog.push(Instructions::Sap(id));
-                prog.push(Instructions::Pta)
+                prog.push(IrInst2::Sap(id.clone()));
+                prog.push(IrInst2::Pta)
             }
-            IrInst::Inp => prog.push(Instructions::Inp),
+            IrInst::Inp => prog.push(IrInst2::Inp),
             IrInst::Pek => {
-                prog.push(Instructions::Pek);
-                prog.push(Instructions::Pop)
+                prog.push(IrInst2::Pek);
+                prog.push(IrInst2::Pop)
             }
-            IrInst::Add => prog.push(Instructions::Add),
-            IrInst::Sub => prog.push(Instructions::Sub),
-            IrInst::Mul => prog.push(Instructions::Mul),
-            IrInst::Div => prog.push(Instructions::Div),
-            IrInst::Mod => prog.push(Instructions::Mod),
-            IrInst::Max => prog.push(Instructions::Max),
-            IrInst::Min => prog.push(Instructions::Min),
+            IrInst::Add => prog.push(IrInst2::Add),
+            IrInst::Sub => prog.push(IrInst2::Sub),
+            IrInst::Mul => prog.push(IrInst2::Mul),
+            IrInst::Div => prog.push(IrInst2::Div),
+            IrInst::Mod => prog.push(IrInst2::Mod),
+            IrInst::Max => prog.push(IrInst2::Max),
+            IrInst::Min => prog.push(IrInst2::Min),
             IrInst::Swap(id0, id1) => {
-                prog.push(Instructions::Sap(id0));
-                prog.push(Instructions::Pfa);
-                prog.push(Instructions::Sap(id1));
-                prog.push(Instructions::Pfa);
-                prog.push(Instructions::Sap(id0));
-                prog.push(Instructions::Pta);
-                prog.push(Instructions::Sap(id1));
-                prog.push(Instructions::Pta)
+                prog.push(IrInst2::Sap(id0.clone()));
+                prog.push(IrInst2::Pfa);
+                prog.push(IrInst2::Sap(id1.clone()));
+                prog.push(IrInst2::Pfa);
+                prog.push(IrInst2::Sap(id0.clone()));
+                prog.push(IrInst2::Pta);
+                prog.push(IrInst2::Sap(id1.clone()));
+                prog.push(IrInst2::Pta)
             }
-            IrInst::Jmp(id) => prog.push(Instructions::Jmp(id)),
-            IrInst::Jiz(id) => prog.push(Instructions::Jiz(id)),
-            IrInst::Jnz(id) => prog.push(Instructions::Jnz(id)),
-            IrInst::Label(id) => _ = lblmgr.insert(id, prog.len()),
+            IrInst::Jmp(id) => prog.push(IrInst2::Jmp(id.clone())),
+            IrInst::Jiz(id) => prog.push(IrInst2::Jiz(id.clone())),
+            IrInst::Jnz(id) => prog.push(IrInst2::Jnz(id.clone())),
+            IrInst::Label(id) => _ = lblmgr.insert(id.clone(), prog.len()),
         }
     }
 }
@@ -145,14 +165,24 @@ impl Ir {
         for inst in &self.0 {
             inst.codegen(&mut prog, &mut lblmgr);
         }
-        for inst in &mut prog {
-            match *inst {
-                Instructions::Jmp(id) => *inst = Instructions::Jmp(*lblmgr.get(&id).unwrap()),
-                Instructions::Jiz(id) => *inst = Instructions::Jiz(*lblmgr.get(&id).unwrap()),
-                Instructions::Jnz(id) => *inst = Instructions::Jnz(*lblmgr.get(&id).unwrap()),
-                _ => (),
-            }
-        }
-        prog
+        prog.iter().map(|inst| match inst {
+            IrInst2::Psh(val) => Instructions::Psh(*val),
+            IrInst2::Sap(id) => Instructions::Sap(*id),
+            IrInst2::Pfa => Instructions::Pfa,
+            IrInst2::Pta => Instructions::Pta,
+            IrInst2::Pek => Instructions::Pek,
+            IrInst2::Pop => Instructions::Pop,
+            IrInst2::Inp => Instructions::Inp,
+            IrInst2::Add => Instructions::Add,
+            IrInst2::Sub => Instructions::Sub,
+            IrInst2::Mul => Instructions::Mul,
+            IrInst2::Div => Instructions::Div,
+            IrInst2::Mod => Instructions::Mod,
+            IrInst2::Max => Instructions::Max,
+            IrInst2::Min => Instructions::Min,
+            IrInst2::Jmp(id) => Instructions::Jmp(*lblmgr.get(id).unwrap()),
+            IrInst2::Jiz(id) => Instructions::Jiz(*lblmgr.get(id).unwrap()),
+            IrInst2::Jnz(id) => Instructions::Jnz(*lblmgr.get(id).unwrap()),
+        }).collect()
     }
 }
